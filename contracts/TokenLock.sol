0bb861b8e5ea9eace0dd55d91f58831728d5f311
merlox <merloxdixcontrol@gmail.com>
2017-11-27 22:01:00 +0200

    Improving the code, not working for some weird reason

--- contracts/TokenLock.sol
+++ contracts/TokenLock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 
 import "./Disbursement.sol";
 import "./SafeMath.sol";
-
+import "./HumanStandardToken.sol";
 
 /**
 this contract should be the address for disbursement contract.
@@ -11,7 +11,6 @@ It should not allow to disburse any token for a given time "initialLockTime"
 lock "50%" of tokens for 10 years.
 transfer 50% of tokens to a given address.
 */
-
 contract TokenLock {
   using SafeMath for uint;
   address public owner;
@@ -21,6 +20,14 @@ contract TokenLock {
   uint public longLock;
 
   uint public shortShare;
+
+  address public levAddress = 0x0f4ca92660efad97a9a70cb0fe969c755439772c;
+
+  address public disbursement;
+
+  bool public isShortSent = false;
+
+  bool public isLongSent = false;
 
   modifier onlyOwner{
     require(msg.sender == owner);
@@ -42,17 +49,47 @@ contract TokenLock {
     shortShare = _shortShare;
   }
 
-  function changeOwner(address _owner) onlyOwner validAddress(_owner) {
+  function setDisbursement(address _disbursement) public onlyOwner {
+    require(_disbursement != address(0));
+    disbursement = _disbursement;
+  }
+
+  // To move the tokens from the Disbursement contract to this one
+  function withdraw() public {
+    uint256 amount = Disbursement(disbursement).calcMaxWithdraw();
+    Disbursement(disbursement).withdraw(address(this), amount);
+  }
+
+  function changeOwner(address _owner) public onlyOwner validAddress(_owner) {
     owner = _owner;
   }
 
-  function transferShortTermTokens(address _wallet) onlyOwner{
-    //current time > shortLock
-    // withdraw shortShare% to _wallet address
+  function transferShortTermTokens(address _wallet) public onlyOwner {
+    require(_wallet != address(0));
+    require(now > shortLock);
+    require(!isShortSent);
+
+    isShortSent = true;
+
+    // 1. Get how many tokens this contract has with a token instance and check this token balance
+    uint256 tokenBalance = HumanStandardToken(levAddress).balanceOf(this);
+    uint256 amountToSend = tokenBalance.mul(shortShare).div(100);
+
+    // 2. Transfer those tokens with the _shortShare percentage
+    HumanStandardToken(levAddress).transfer(_wallet, amountToSend);
   }
 
-  function transferLongTermTokens(address _wallet) onlyOwner{
-    //current time > longLock
-    // withdraw all to _wallet address
+  function transferLongTermTokens(address _wallet) public onlyOwner {
+    require(_wallet != address(0));
+    require(now > longLock);
+    require(!isLongSent);
+
+    isLongSent = true;
+
+    // 1. Get how many tokens this contract has with a token instance and check this token balance
+    uint256 tokenBalance = HumanStandardToken(levAddress).balanceOf(this);
+
+    // 2. Transfer those tokens with the _shortShare percentage
+    HumanStandardToken(levAddress).transfer(_wallet, tokenBalance);
   }
 }
--- test/token-lock.spec.js
+++ test/token-lock.spec.js
@@ -9,6 +9,8 @@ describe('token-lock', function () {
   this.timeout(10000);
   let owner, token, tokenLock, disbursement;
   let current;
+	let oneYear = 365*24*60*60
+
   before(async function () {
     await testrpc.start();
   });
@@ -16,10 +18,18 @@ describe('token-lock', function () {
   beforeEach(async function(){
     owner = testrpc.account(0).address;
     token = await lib.deploy(Token.abi, Token.bytecode, owner, [1e12, 'LEV', 9, 'LEV', owner]);
-    tokenLock = await lib.deploy(TokenLock.abi, TokenLock.bytecode, owner, [owner, 365*24*60*60, 10*365*24*60*60, 50]);
+    tokenLock = await lib.deploy(TokenLock.abi, TokenLock.bytecode, owner, [owner, oneYear, 10*oneYear, 50]);
     current = await lib.getCurrentTime();
-    disbursement = await lib.deploy(Disbursement.abi, Disbursement.bytecode, owner, [tokenLock._address, 365 * 24 * 60 * 60, current]);
+    disbursement = await lib.deploy(Disbursement.abi, Disbursement.bytecode, owner, [tokenLock._address, oneYear, current]);
+
+		await disbursement.methods.setup(token._address).send({
+			from: owner
+		});
+		await tokenLock.methods.setDisbursement(disbursement._address).send({
+			from: owner
+		});
   });
+
   afterEach(async function(){
     await testrpc.reset();
   });
@@ -33,15 +43,106 @@ describe('token-lock', function () {
   });
 
   it('should not be able to disburse before 1 year', async function () {
+		ownerBalance = await token.methods.balanceOf(owner).call();
 
+		// Send the token to disbursement
+		await token.methods.transfer(disbursement._address, await token.methods.balanceOf(owner).call()).send({
+			from: owner
+		});
+
+		// Advance in time to be able to withdraw from Disbursement to TokenLock half a year
+		await lib.forceTime(await lib.getCurrentTime() + oneYear / 2);
+		await tokenLock.methods.withdraw().send({
+			from: owner
+		});
+
+		// The transfer short term tokens should fail
+		try {
+			// Execute transferShortTermTokens(owner)
+			await tokenLock.methods.transferShortTermTokens(owner).send({
+				from: owner
+			});
+			expect.fail();
+		} catch (e) {
+			// Check owner balance
+			finalOwnerBalance = await token.methods.balanceOf(owner).call();
+			expect(finalOwnerBalance).toEqual(0);
+		}
   });
   it('should be able to disburse 50% of tokens after 1 year', async function () {
+		ownerBalance = await token.methods.balanceOf(owner).call();
 
+		// Send the token to disbursement
+		await token.methods.transfer(disbursement._address, await token.methods.balanceOf(owner).call()).send({
+			from: owner
+		});
+
+		// Advance in time to be able to withdraw from Disbursement to TokenLock after 1 year + 1 second
+		await lib.forceTime(await lib.getCurrentTime() + oneYear + 1);
+		await tokenLock.methods.withdraw().send({
+			from: owner
+		});
+
+		// Execute transferShortTermTokens(owner)
+		await tokenLock.methods.transferShortTermTokens(owner).send({
+			from: owner
+		});
+
+		// Check owner balance
+		finalOwnerBalance = await token.methods.balanceOf(owner).call();
+		expect(finalOwnerBalance).toEqual(ownerBalance / 2);
   });
   it('should not be able to disburse more than 50% of tokens before 10 years', async function () {
+		ownerBalance = await token.methods.balanceOf(owner).call();
 
+		// Send the token to disbursement
+		await token.methods.transfer(disbursement._address, await token.methods.balanceOf(owner).call()).send({
+			from: owner
+		});
+
+		// Advance in time to be able to withdraw from Disbursement to TokenLock after 5 years
+		await lib.forceTime(await lib.getCurrentTime() + oneYear * 5);
+		await tokenLock.methods.withdraw().send({
+			from: owner
+		});
+		await tokenLock.methods.transferShortTermTokens(owner).send({
+			from: owner
+		});
+
+		// Shouldn't transfer the long term tokens before the 10 years
+		try {
+			await tokenLock.methods.transferLongTermTokens(owner).send({
+				from: owner
+			});
+			expect.fail();
+		} catch(e) {
+			// Check owner balance
+			finalOwnerBalance = await token.methods.balanceOf(owner).call();
+			expect(finalOwnerBalance).toEqual(ownerBalance / 2);
+		}
   });
   it('should not be able to disburse rest of the tokens after 10 years', async function () {
+		ownerBalance = await token.methods.balanceOf(owner).call();
 
+		// Send the token to disbursement
+		await token.methods.transfer(disbursement._address, await token.methods.balanceOf(owner).call()).send({
+			from: owner
+		});
+
+		// Advance in time to be able to withdraw from Disbursement to TokenLock after 10 years + 1 second
+		await lib.forceTime(await lib.getCurrentTime() + oneYear * 10 + 1);
+		await tokenLock.methods.withdraw().send({
+			from: owner
+		});
+		await tokenLock.methods.transferShortTermTokens(owner).send({
+			from: owner
+		});
+		await tokenLock.methods.transferLongTermTokens(owner).send({
+			from: owner
+		});
+
+		// Check owner balance
+		finalOwnerBalance = await token.methods.balanceOf(owner).call();
+		expect(finalOwnerBalance).toEqual(ownerBalance);
   });
 });
